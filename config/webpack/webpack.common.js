522cd53c477a9bde4a51d9caab4f3b15a73cda96
yari <yarlaz@gmail.com>
2017-04-27 10:45:16 +0200

    chore: add image compression

--- config/webpack/webpack.common.js
+++ config/webpack/webpack.common.js
@@ -6,6 +6,8 @@ const CommonsChunkPlugin = require('webpack/lib/optimize/CommonsChunkPlugin');
 const ContextReplacementPlugin = require('webpack/lib/ContextReplacementPlugin');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const ScriptExtHtmlWebpackPlugin = require('script-ext-html-webpack-plugin');
+const CopyWebpackPlugin = require('copy-webpack-plugin');
+const ImageminPlugin = require('imagemin-webpack-plugin').default
 
 const helpers = require('../helpers');
 
@@ -43,7 +45,6 @@ module.exports = {
       {
         test: /\.css$/,
         use: ['to-string-loader', 'css-loader'],
-        // styles.scss will hold the global styles
         exclude: [helpers.root('src/styles')]
       },
       {
@@ -56,8 +57,54 @@ module.exports = {
         use: 'raw-loader',
         exclude: [helpers.root('src/index.html')]
       },
+      /*
       {
-        test: /\.(png|jpe?g|gif|svg|woff|woff2|ttf|eot|ico)$/,
+        // I could use this configuration to copy and compress images
+        // but I should use `import '../assets/img/imagename.jpg'` for each image I use
+        // it is probably better to simply copy all the assets folder
+        test: /\.(png|jpe?g|gif|svg)$/,
+        use: [
+          {
+            loader: 'file-loader',
+            options: {
+              name: 'assets/img/[name].[ext]'
+            }
+          },
+          {
+            loader: 'image-webpack-loader',
+            options: {
+              mozjpeg: {
+                quality: 65
+              },
+              pngquant:{
+                quality: "10-20",
+                speed: 4
+              },
+              svgo:{
+                plugins: [
+                  {
+                    removeViewBox: false
+                  },
+                  {
+                    removeEmptyAttrs: false
+                  }
+                ]
+              },
+              gifsicle: {
+                optimizationLevel: 7,
+                interlaced: false
+              },
+              optipng: {
+                optimizationLevel: 7,
+                interlaced: false
+              }
+            }
+          }
+        ]
+      },
+      */
+      {
+        test: /\.(png|jpe?g|gif|eot|ico)$/,
         use: 'file-loader'
       },
       {
@@ -68,7 +115,6 @@ module.exports = {
   },
   plugins: [
     new AssetsPlugin({
-      // where to save the fil
       path: helpers.root('dist'),
       filename: 'webpack.assets.json',
       prettyPrint: true
@@ -76,6 +122,27 @@ module.exports = {
     new CheckerPlugin(),
     new CommonsChunkPlugin({
       name: ['polyfills', 'vendor', 'main'].reverse()
+    }),
+    new CopyWebpackPlugin([
+      { from: 'src/assets', to: 'assets' }
+    ]),
+    new ImageminPlugin({
+      disable: !PROD,
+      mozjpeg: {
+        quality: 65
+      },
+      pngquant:{
+        quality: "10-20",
+        speed: 4
+      },
+      gifsicle: {
+        optimizationLevel: 7,
+        interlaced: false
+      },
+      optipng: {
+        optimizationLevel: 7,
+        interlaced: false
+      }
     }),
     // Workaround for angular/angular#11580
     new ContextReplacementPlugin(
