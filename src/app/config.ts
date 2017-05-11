e1a278c2131f6e3671e25c9eaf721ec608371667
yari <yarlaz@gmail.com>
2017-05-11 17:41:01 +0200

    chore: add firebase env variable config

--- src/app/config.ts
+++ src/app/config.ts
@@ -1,9 +1,11 @@
+import { config } from 'firebase-functions';
+
 export const config = {
   usda: {
-    apiKey: process.env.USDA_API || 'YOUR_USDA_API_KEY'
+    apiKey: process.env.USDA_API || config().usda.api || 'YOUR_USDA_API_KEY'
   },
   edamam: {
-    apiKey: process.env.EDAMAM_KEY || 'YOUR_EDAMAM_API_KEY',
-    appId: process.env.EDAMAM_APP_ID || 'YOUR_EDAMAM_APP_ID'
+    apiKey: process.env.EDAMAM_KEY || config().edamam.key || 'YOUR_EDAMAM_API_KEY',
+    appId: process.env.EDAMAM_APP_ID || config().edamam.id || 'YOUR_EDAMAM_APP_ID'
   }
 };
